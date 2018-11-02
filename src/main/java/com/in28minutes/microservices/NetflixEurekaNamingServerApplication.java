502a587770b16b4022d15818d41db6f389058767
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-11-02 09:14:05 -0400

    Commit Inicial

---
+++ src/main/java/com/in28minutes/microservices/NetflixEurekaNamingServerApplication.java
@@ -0,0 +1,12 @@
+package com.in28minutes.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
+	}
+}
